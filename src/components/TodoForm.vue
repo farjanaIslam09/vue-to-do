82fd51d13177844b790933c99db55f3532ee6f4f
farjanaIslamNipa <farjana.jatri@gmail.com>
2023-08-10 15:22:44 +0600

    added pagination

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="h-screen w-full pt-10 bg-gray-100">
     <div class="w-1/3 mx-auto h-auto bg-white rounded-md shadow p-5">
-      <!-- <h2 class="text-3xl font-medium text-center pb-3 border-b">To Do List</h2> -->
+      <h2 class="text-3xl font-medium text-center pb-3 border-b">To Do List</h2>
       <div class="mt-4 pb-3">
         <form @submit.prevent="addTask" class="flex items-center pb-1">
           <input v-model.trim="task" type="text" class="w-full border rounded-md px-3 py-2 focus:none outline-none" placeholder="Enter your to do list">
-          <!-- <input type="submit"  class="ml-2 bg-purple-900 text-white rounded-md py-2 px-4 cursor-pointer" value="Add"> -->
+          <input type="submit"  class="ml-2 bg-purple-900 text-white rounded-md py-2 px-4 cursor-pointer" value="Add">
         </form>
         <div>
           <p v-if="error.emptyTodo" class="text-red-600 pb-4">{{ error.emptyTodo }}</p>
